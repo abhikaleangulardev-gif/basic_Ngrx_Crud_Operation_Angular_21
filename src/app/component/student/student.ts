8897b0feff9929ae80eb3fdd0adb632238a01cc3
abhishek.kale <abhisahebpatil@gmail.com>
2026-03-14 09:33:54 +0530

    completed-student-component-code

--- src/app/component/student/student.ts
+++ src/app/component/student/student.ts
@@ -1,11 +1,40 @@
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { addStudent, deleteStudent } from '../../student/student.actions';
+import { Student } from '../../student/student.model';
+
 
 @Component({
   selector: 'app-student',
-  imports: [],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
-export class Student {
+export class StudentComponent {
+  student$: Observable<Student[]>;
+
+  constructor(private store: Store<{ students: Student[] }>) {
+    this.student$ = this.store.select(state => state.students);
+  }
+
+
+
+  addStudent() {
+    const student = {
+      id: Date.now(),
+      name: 'Rahul',
+      marks: 80
+    };
+
+    this.store.dispatch(addStudent({ student }));
+  }
+
+  deleteStudent(id: number) {
+    this.store.dispatch(deleteStudent({ id }))
+  }
+
+
 
 }
